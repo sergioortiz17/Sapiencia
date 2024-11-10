c5c0196b5af9e63a35674065e047eb01940a604e
sergioStori <142939469+sergioStori@users.noreply.github.com>
2024-11-10 18:15:00 -0300

    Initial commit

---
+++ FinalProject/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using FinalProject.Models;
+
+namespace FinalProject.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    // Acción para la vista de Matemáticas
+    public IActionResult Matematicas()
+    {
+        ViewData["Message"] = "Esta es la página de Matemáticas.";
+        return View();
+    }
+
+    // Acción para la vista de Arquitectura
+    public IActionResult Arquitectura()
+    {
+        ViewData["Message"] = "Esta es la página de Arquitectura.";
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
